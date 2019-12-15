45f35d00ff9979ee11fb749b7950a66f4d069d8b
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-12-15 21:17:24 +0530

    Update and rename havoc_X01BD.mk to du_X01BD.mk

--- du_X01BD.mk
+++ du_X01BD.mk
@@ -20,16 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Havoc-OS stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-# Official Havoc-OS
-export export HAVOC_BUILD_TYPE=Official
+
+
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_X01BD
+PRODUCT_NAME := du_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
