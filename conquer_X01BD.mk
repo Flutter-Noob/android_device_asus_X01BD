252d97628469112d755d8d9719573f2779a9aff0
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-12-28 12:28:39 +0530

    conquer-ify (2)

--- conquer_X01BD.mk
+++ conquer_X01BD.mk
@@ -19,17 +19,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Havoc-OS stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Conquer-OS stuff
+$(call inherit-product, vendor/conquer/config/common_full_phone.mk)
 
-# Official Havoc-OS
-export export HAVOC_BUILD_TYPE=Official
+# Inherit some common ConquerOS stuff.
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_GAPPS_ARCH := arm64
+
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_X01BD
+PRODUCT_NAME := conquer_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
